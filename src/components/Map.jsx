a0425623e14cb5c334503704f3ccf64efadc9240
Square <aayushk4980@gmail.com>
2024-05-13 14:47:34 +0530

    Added custom location icon

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import locationIcon from "../images/icon-location.svg";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function Map(props) {
+
+    const customIcon = new L.Icon({
+        iconUrl: locationIcon,
+        iconRetinaUrl: locationIcon,
+        iconAnchor: null,
+        popupAnchor: null,
+        shadowUrl: null,
+        shadowSize: null,
+        shadowAnchor: null,
+        iconSize: new L.Point(40, 50)
+    })
 
     return (
         <MapContainer center={props.position} zoom={13} style={{ height: "60vh", width: "100%" }}>
@@ -10,7 +23,7 @@ function Map(props) {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
-            <Marker position={props.position}></Marker>
+            <Marker position={props.position} icon={customIcon}></Marker>
         </MapContainer>
     )
 }
