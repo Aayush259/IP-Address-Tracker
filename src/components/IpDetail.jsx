754adeb91699f05803455d3b0dd374bc7dd325f5
Square <aayushk4980@gmail.com>
2024-05-14 15:18:47 +0530

    Added responsive design

--- src/components/IpDetail.jsx
+++ src/components/IpDetail.jsx
@@ -8,28 +8,28 @@ function IpDetail(props) {
             <div id="ipAddress" className="flex">
                 <div>
                     <div className="title bold-700">IP Address</div>
-                    <div className="value bold-500">{props.ipValue}</div>
+                    <div className="value bold-500" style={{wordWrap: 'break-word', overflowWrap: 'break-word', width: '100%', maxWidth: '156px'}}>{props.ipValue}</div>
                 </div>
                 <div className="divider"></div>
             </div>
             <div id="location" className="flex">
                 <div>
                     <div className="title bold-700">Location</div>
-                    <div className="value bold-500">{props.locationValue}</div>
+                    <div className="value bold-500" style={{wordWrap: 'break-word', overflowWrap: 'break-word', width: '100%', maxWidth: '156px'}}>{props.locationValue}</div>
                 </div>
                 <div className="divider"></div>
             </div>
             <div id="timeZone" className="flex">
                 <div>
                     <div className="title bold-700">Timezone</div>
-                    <div className="value bold-500">{props.timezoneValue}</div>
+                    <div className="value bold-500" style={{wordWrap: 'break-word', overflowWrap: 'break-word', width: '100%', maxWidth: '156px'}}>{props.timezoneValue}</div>
                 </div>
                 <div className="divider"></div>
             </div>
             <div id="isp" className="flex">
                 <div>
                     <div className="title bold-700">Isp</div>
-                    <div className="value bold-500">{props.ispValue}</div>
+                    <div className="value bold-500" style={{wordWrap: 'break-word', overflowWrap: 'break-word', width: '100%', maxWidth: '156px'}}>{props.ispValue}</div>
                 </div>
             </div>
         </div>
