a6b39d323fc0d6ce239dbfeb70c19fc68d771262
Square <aayushk4980@gmail.com>
2024-06-11 17:23:44 +0530

    Added Context and RouterProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='' element={<App />}>
+            <Route path='/' element={<App />} />
+        </Route>
+    )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <RouterProvider router={router} />
 )
